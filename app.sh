0e118d3bc998f58c9f43113307f35d83048e8026
Mike Steder <steder@gmail.com>
2014-02-03 13:16:37 -0600

    removing credentials

--- app.sh
+++ app.sh
@@ -16,9 +16,8 @@ echo "ROOT=$ROOT"
 export ENV=production
 export NEW_RELIC_CONFIG_FILE="$ROOT/etc/newrelic.ini"
 
-#UPDATE THESE
-export AWS_ACCESS_KEY_ID="AKIAIS2WMVOQL3APRCBA"
-export AWS_SECRET_ACCESS_KEY="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"
-export MEMCACHED="cache1a-production.aws.threadless.com:11211;cache1e-production.aws.threadless.com:11211"
+if [ -e $ROOT/conf.sh ]; then
+    source $ROOT/conf.sh
+fi
 
 /opt/app/env/bin/newrelic-admin run-program /opt/app/env/bin/gunicorn -k gevent -c etc/gunicorn.conf.py giraffe:app --log-level=DEBUG
